57c223ef78d317b72398c744b9aa04b587e8cafd
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2022-07-27 07:41:56 -0400

    more debug code

--- src/core/linear-algebra/internal/Tines_FindAmax_Internal.hpp
+++ src/core/linear-algebra/internal/Tines_FindAmax_Internal.hpp
@@ -69,7 +69,13 @@ namespace Tines {
             }
           },
           reducer_value);
-        Kokkos::single(Kokkos::PerTeam(member), [&]() { *idx = value.loc; });
+        Kokkos::single(Kokkos::PerTeam(member), [&]() {
+            if(value.loc > m){
+                throw std::invalid_argument("MaxLocation Not Found." + std::to_string(value.loc) + " : " + std::to_string(value.val));
+            }
+
+            *idx = value.loc;
+        });
       } else {
         Kokkos::single(Kokkos::PerTeam(member), [&]() { *idx = 0; });
       }
